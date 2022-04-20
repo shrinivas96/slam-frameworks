2a59d4a7afeaff70b914de4163f19e1cdbb1dc38
shrinivas96 <shrinivasiyengar@live.com>
2022-04-20 18:15:11 +0200

    Added read odometry and Updated read laser Updated each reading to be an object of laser or odometry class instead of a dictionary

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,3 +1,4 @@
+from tools import laserReading, odomReading
 import numpy as np
 import tools
 
@@ -5,31 +6,24 @@ import tools
 def read_robotlaser(tokens):
     # order of each line of ROBOTLASER1:
     # ROBOTLASER1 laser_type start_angle field_of_view angular_resolution maximum_range accuracy remission_mode num_readings [range_readings] laser_pose_x laser_pose_y laser_pose_theta robot_pose_x robot_pose_y robot_pose_theta laser_tv laser_rv forward_safety_dist side_safty_dist
+    # ipc_timestamp ipc_hostname logger_timestamp
     
     # all tokens converted into float values
     numTokens = tools.str2double(tokens)
 
     # empty skeleton
-    currentReading = {
-        "start_angle"       : 0,
-        "angular_resolution": 0,
-        "maximum_range"     : 0,
-        "ranges"            : np.array([]),
-        "pose"              : np.zeros((3,1)),
-        "laser_offset"      : np.zeros((3,1)),
-        "timestamp"         : 0
-    }
+    currentReading = laserReading()
 
     # to track the values present in each line
     col = 2 
 
-    currentReading["start_angle"] = numTokens[col]
+    currentReading.start_angle = numTokens[col]
     col += 2                        # 1: next, 1: skip FOV
 
-    currentReading["angular_resolution"] = numTokens[col]
+    currentReading.angular_resolution = numTokens[col]
     col += 1
 
-    currentReading["maximum_range"] = numTokens[col]
+    currentReading.maximum_range = numTokens[col]
     col += 3                        # 1: next, 2: skip accuracy, remission_mode
 
     # the next numReadings values in the line are the laser ranges
@@ -37,7 +31,7 @@ def read_robotlaser(tokens):
     col += 1                        # 1: next
 
     # for reasons unknown, the original script from freiburg took 
-    currentReading["ranges"] = numTokens[col:col+numReadings]
+    currentReading.ranges = numTokens[col:col+numReadings]
     col += numReadings
 
     numRemissions = int(numTokens[col])
@@ -46,28 +40,52 @@ def read_robotlaser(tokens):
     laserPose = numTokens[col:col+3]
     col += 3                        # 3: laser pose
 
-    currentReading["pose"] = numTokens[col:col+3]
+    currentReading.pose = numTokens[col:col+3]
     col += 3                        # 3: robot pose
 
     # !! << this is not verified yet >> !!
     # translating the laser end points based on the current pose
-    laserEndPtsTrans = np.matmul(tools.v2t(currentReading["pose"]), tools.v2t(laserPose))
+    laserEndPtsTrans = np.matmul(tools.v2t(currentReading.pose), tools.v2t(laserPose))
     invLaserOffset = np.linalg.inv(laserEndPtsTrans)
-    currentReading["laser_offset"] = tools.t2v(invLaserOffset)
+    currentReading.laser_offset = tools.t2v(invLaserOffset)
     col += 5
 
-    currentReading["timestamp"] = numTokens[col]
+    currentReading.timestamp = numTokens[col]
 
     return currentReading
 
 
-def read_odom():
-    pass
+def read_odom(tokens):
+    # order of each line of ODOM:
+    # ODOM x y theta tv rv accel
+    # ipc_timestamp ipc_hostname logger_timestamp
+    
+    # all tokens converted into float values
+    numTokens = tools.str2double(tokens)
+    
+    # empty skeleton instantiated
+    currentOdometry = odomReading()
+
+    col = 1
+
+    currentOdometry.state = numTokens[col:col+3]
+    col += 3                            # x, y, theta
+
+    currentOdometry.ctrl = numTokens[col:col+2]
+    col += 2                            # tv, rv
+
+    currentOdometry.accel = numTokens[col]
+    col += 1                            # accel
+
+    currentOdometry.timestamp = numTokens[col]
+
+    return currentOdometry
 
 
 def read_data(filePath):
     # empty array to place all readings
     laser = np.array([])
+    odom = np.array([])
 
     # retrieve data from file
     with open(filePath) as f:
@@ -83,16 +101,14 @@ def read_data(filePath):
         if tokens[0] == "ROBOTLASER1":
             currentReading = read_robotlaser(tokens)
             laser = np.append(laser, currentReading)
-        elif tokens[0] == "":
-            currentOdometry = read_odom()
-
+        elif tokens[0] == "ODOM":
+            currentOdometry = read_odom(tokens)
+            odom = np.append(odom, currentOdometry)
         lineCount +=1
-
-
-def main():
-    filePath = "freiburg-dataset/mit-csail-3rd-floor-2005-12-17-run4.log"
-    read_data(filePath)
+    
+    return laser, odom
 
 
 if __name__ == "__main__":
-    main()
+    filePath = "csail-dataset/mit-csail-3rd-floor-2005-12-17-run4.log"
+    laser, odom = read_data(filePath)
