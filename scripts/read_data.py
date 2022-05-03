e3b289ee0b85bdf2200ce8f39d2a408970030b23
shrinivas96 <shrinivasiyengar@live.com>
2022-05-03 22:27:54 +0200

    Added function to read FLASER tag from log files. FLASER is almost similar to ROBOTLASER. Added some comments and chaged some formatting.

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,4 +1,13 @@
-from tools import laserReading, odomReading
+"""
+The main file to read data from the files. 
+Files can contain a stream of data after a tag such as "ODOM" or "ROBOTLASER".
+It is not general enough yet, as not all files have the same tags, and needs 
+some manual modifications. The last three values in almost each line is:
+
+ipc_timestamp ipc_hostname logger_timestamp
+"""
+
+from tools import laserReading, odomReading, flaserReading
 import numpy as np
 import tools
 
@@ -7,12 +16,12 @@ def read_robotlaser(tokens):
     # order of each line of ROBOTLASER1:
     # ROBOTLASER1 laser_type start_angle field_of_view angular_resolution maximum_range accuracy remission_mode num_readings [range_readings] laser_pose_x laser_pose_y laser_pose_theta robot_pose_x robot_pose_y robot_pose_theta laser_tv laser_rv forward_safety_dist side_safty_dist
     # ipc_timestamp ipc_hostname logger_timestamp
-    
+
     # all tokens converted into float values
     numTokens = tools.str2double(tokens)
 
     # to track the values present in each line
-    col = 2 
+    col = 2
 
     start_angle = numTokens[col]
     col += 2                        # 1: next, 1: skip FOV
@@ -27,7 +36,7 @@ def read_robotlaser(tokens):
     numReadings = int(numTokens[col])
     col += 1                        # 1: next
 
-    # for reasons unknown, the original script from freiburg took 
+    # for reasons unknown, the original script from freiburg took
     ranges = numTokens[col:col+numReadings]
     col += numReadings
 
@@ -49,7 +58,8 @@ def read_robotlaser(tokens):
 
     timestamp = numTokens[col]
 
-    currentReading = laserReading(start_angle, angular_resolution, maximum_range, ranges, pose, laser_offset, timestamp)
+    currentReading = laserReading(
+        start_angle, angular_resolution, maximum_range, ranges, pose, laser_offset, timestamp)
 
     return currentReading
 
@@ -58,10 +68,10 @@ def read_odom(tokens):
     # order of each line of ODOM:
     # ODOM x y theta tv rv accel
     # ipc_timestamp ipc_hostname logger_timestamp
-    
+
     # all tokens converted into float values
     numTokens = tools.str2double(tokens)
-    
+
     col = 1
 
     state = numTokens[col:col+3]
@@ -78,6 +88,35 @@ def read_odom(tokens):
     currentOdometry = odomReading(state, ctrl, accel, timestamp)
 
     return currentOdometry
+
+
+def read_flaser(tokens):
+    # order of each line of FLASER:
+    # FLASER num_readings [range_readings] x y theta odom_x odom_y odom_theta
+    # ipc_timestamp ipc_hostname logger_timestamp
+
+    # all tokens converted into float values
+    numTokens = tools.str2double(tokens)
+
+    col = 1
+
+    numReadings = int(numTokens[col])
+    col += 1
+
+    ranges = numTokens[col:col+numReadings]
+    col += numReadings
+
+    pose = numTokens[col:col+3]
+    col += 3
+
+    odomState = numTokens[col:col+3]
+    col += 3
+
+    timestamp = numTokens[col]
+
+    currentFLASER = flaserReading(ranges, pose, odomState, timestamp)
+
+    return currentFLASER
 
 
 def read_data(filePath):
@@ -102,11 +141,14 @@ def read_data(filePath):
         elif tokens[0] == "ODOM":
             currentOdometry = read_odom(tokens)
             odom = np.append(odom, currentOdometry)
-        lineCount +=1
-    
+        elif tokens[0] == "FLASER":
+            currentFLASER = read_flaser(tokens)
+            laser = np.append(laser, currentFLASER)
+        lineCount += 1
+
     return laser, odom
 
 
 if __name__ == "__main__":
-    filePath = "csail-dataset/mit-csail-3rd-floor-2005-12-17-run4.log"
+    filePath = "dataset/csail-dataset/mit-csail-3rd-floor-2005-12-17-run4.log"
     laser, odom = read_data(filePath)
--- scripts/tools.py
+++ scripts/tools.py
@@ -1,3 +1,8 @@
+"""
+A list of functions to be used across the project and 
+some classes to hold data regarding laser data and odometry.
+"""
+
 import numpy as np
 
 
@@ -44,19 +49,24 @@ def velMotionModel(state, ctrlCmd):
     deltaT = 0.1                    # time for which these velocities are executed
     v = ctrlCmd[0]                  # translation velocity
     w = ctrlCmd[1]                  # rotational velocity (omega)
-    v_by_w = v/w                    # for reusability
+    arcRadius = v[0]/w[0]             # radius of arc created by applying rot and trans velocity
+    if np.isnan(arcRadius):
+        print(sep="", end="")
+        arcRadius = 0
     theta = state[2]
     
-    update = np.array([(-v_by_w*np.sin(theta) + v_by_w*np.sin(theta + w*deltaT))[0],
-                        (v_by_w*np.cos(theta) - v_by_w*np.cos(theta + w*deltaT))[0],
-                        w*deltaT]).reshape((3, 1))
+    update = np.array([(-arcRadius*np.sin(theta) + arcRadius*np.sin(theta + w*deltaT))[0],
+                        (arcRadius*np.cos(theta) - arcRadius*np.cos(theta + w*deltaT))[0],
+                        (w*deltaT)[0]]).reshape((3, 1))
     
     state += update
     return state
 
 
 class laserReading():
-    # empty skeleton to hold data about each sensor reading
+    # ROBOTLASER1 laser_type start_angle field_of_view angular_resolution maximum_range accuracy remission_mode ...
+    # num_readings [range_readings] laser_pose_x laser_pose_y laser_pose_theta robot_pose_x robot_pose_y robot_pose_theta 
+    # laser_tv laser_rv forward_safety_dist side_safty_dist
     def __init__(self, start_angle, ang_res, max_range, ranges, pose, laser_offset, timestamp) -> None:
         self.start_angle = start_angle
         self.angular_resolution = ang_res
@@ -71,6 +81,7 @@ class laserReading():
         print("Start angle: \t", self.start_angle)
         print("Ang Res: \t", self.angular_resolution)
         print("Max Range: \t", self.maximum_range)
+        print("Ranges size: \t", self.ranges.size)
         print("Ranges: \t", self.ranges[:4], "...", self.ranges[-4:])
         print("Pose: \t\t", self.pose)
         print("Timestamp: \t", self.timestamp)
@@ -93,6 +104,23 @@ class odomReading():
         print("Values for laser reading:")
         print("State: \t\t", self.state.reshape((3,)))
         print("Control: \t", self.ctrl.reshape((2,)))
+        print("Timestamp: \t", self.timestamp)
+
+
+class flaserReading():
+    # FLASER num_readings [range_readings] x y theta odom_x odom_y odom_theta
+    def __init__(self, ranges, pose, odomState, timestamp) -> None:
+        self.ranges = ranges
+        self.pose = pose.reshape((3,1))
+        self.odomState = odomState.reshape((3,1))
+        self.timestamp = timestamp
+
+    def getter(self):
+        print("Values for laser reading:")
+        print("Ranges size: \t", self.ranges.size)
+        print("Ranges: \t", self.ranges[:4], "...", self.ranges[-4:])
+        print("Pose: \t\t", self.pose)
+        print("Odo State: \t\t", self.odomState)
         print("Timestamp: \t", self.timestamp)
 
 
